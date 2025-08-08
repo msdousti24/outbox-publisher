0b19085c3f8cc5e7ae14c8de3af6156641d48ebd
Sadeq <155274977+msdousti24@users.noreply.github.com>
2025-08-08 15:01:37 +0200

    Create outpost project

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,10 +30,21 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-jooq")
+    implementation("org.postgresql:postgresql:42.7.7")
+
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.9.1")
+    testImplementation("io.mockk:mockk:1.14.5")
+    testImplementation("com.ninja-squad:springmockk:4.0.2")
 }
 
 tasks.withType<Test> {
